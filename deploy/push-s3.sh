8ebebe7fec34c70c07b980521e071061b8a41119
Tim Tiemens <timtiemens@fakeusers.noreply.github.com>
2019-04-20 20:14:22 -0500

    protect against sh

--- deploy/push-s3.sh
+++ deploy/push-s3.sh
@@ -1,6 +1,13 @@
-#!/bin/bash
+#!/bin/bash -e
 
 CURRENT=`pwd`
+
+# protection for the "DIR=$( ).." line below:
+if [ "$BASH" != "/bin/bash" ]
+then
+    echo "BASH is not /bin/bash - either use ./push-s3.sh  or bash push-s3.sh"
+    exit 1
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -12,8 +19,8 @@ then
     exit 1
 fi
 
-
 cd $WWW
+
 aws s3 sync --profile admins3 . s3://www.timtiemens.com --exclude "*~"
 
 
